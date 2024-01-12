5a2d911f62fb4ca6d67ed5bb0fa4f753fc2c6ad4
omegatech-andre <omega.ti_ind@outlook.com>
2024-01-12 16:01:59 -0300

    feat:{add views}

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom'
 import './index.scss'
+import { Link } from 'react-router-dom'
 
 export default function NavBar({ id }) {
   return (
     <>
       <div className="navbar">
-        <p>minha navbar</p>
-        <div className="links">
-        <Link to='/books'>allbooks</Link>
-        <Link to={`/book/${id}`}>onebooks</Link>
-        <Link to='/book/create'>createbook</Link>
-        <Link to={`/book/update/${id}`}>updatebook</Link>
+        <div className="navbar__logo">
+          <img src="/assets/logo.png" alt="logo DNC" />
+        </div>
+        <div className="navbar__links">
+          <Link to='/'>Home</Link>
+          <Link to='/books'>Livros</Link>
         </div>
       </div>
     </>
   )
 }
+
+
+{/* 
+  <Link to={`/book/${id}`}>onebooks</Link>
+  <Link to='/book/create'>createbook</Link>
+  <Link to={`/book/update/${id}`}>updatebook</Link>
+*/}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <>
       <div className="home">
-        <p>Home</p>
+        <div className="home__img">
+          <img src="/assets/books.png" alt="" />
+        </div>
+        <div className="home__content">
+          <h1>Biblioteca DNC</h1>
+          <p>Um acervo de E-Books gratuitos, baixe e aproveite</p>
+        </div>
       </div>
     </>
   )
