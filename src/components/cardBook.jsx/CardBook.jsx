37824efde1957a5e4faed5bf6b822c2865e851f9
omegatech-andre <omega.ti_ind@outlook.com>
2024-01-16 09:08:20 -0300

    fix:{ajustes finos 1}

--- src/components/cardBook.jsx/CardBook.jsx
+++ src/components/cardBook.jsx/CardBook.jsx
@@ -8,7 +8,7 @@ import ModalDelete from '../modals/modalDelete'
 export default function CardBook({ link, book }) {
   const [openModalEdit, setOpenModalEdit] = useState(false)
   const [openModalDelete, setOpenModalDelete] = useState(false)
-  
+
   return (
     <>
       {!link ? (
@@ -19,25 +19,25 @@ export default function CardBook({ link, book }) {
             <p><span>Autor:</span>{book.publisher}</p>
             <p><span>Código ISBN:</span>{book.isbn}</p>
             <p><span>Número de páginas:</span>{book.pages}</p>
-            {book.sumary &&
-              <>
-                <span>Descrição:</span>
-                <div className="cardbook__details--sumary">
+            <span>Descrição:</span>
+            <div className="cardbook__details--sumary">
+              {book.sumary &&
+                <>
                   <p>{book.sumary}</p>
-                </div>
-              </>
-            }
+                </>
+              }
+            </div>
           </div>
           <div className="cardbook__btnshow">
             <FiEdit size={23} onClick={() => setOpenModalEdit(!openModalEdit)} />
-            <FiTrash size={23} onClick={() => setOpenModalDelete(!openModalDelete)}  />
+            <FiTrash size={23} onClick={() => setOpenModalDelete(!openModalDelete)} />
           </div>
           <div className="cardbook__modals">
             <ModalEdit isOpen={openModalEdit} isClose={setOpenModalEdit} bookdata={book} />
             <ModalDelete isOpen={openModalDelete} isClose={setOpenModalDelete} />
           </div>
         </div>
-      ):(
+      ) : (
         <Link to={link} className='cardbookpreview'>
           <div className="cardbookpreview__details">
             <h2>{book.title}</h2>
