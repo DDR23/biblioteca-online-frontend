5d177818e5cc44744bb920fee4d151d28ce7531a
DDR23 <roboreserva.andre@gmail.com>
2024-01-16 00:52:00 -0300

    fix:{alterando input de value para defaultvalue}

--- src/components/modals/ModalEdit.jsx
+++ src/components/modals/ModalEdit.jsx
@@ -101,23 +101,23 @@ export default function Modal({ isOpen, isClose, bookdata }) {
             <div className="modal__content--close">
               <IoCloseCircleOutline size={23} onClick={() => isClose(!isOpen)} />
             </div>
-            <form onSubmit={handleSubmit(updateOneBook)}>
+            <form onSubmit={handleSubmit(updateOneBook)} >
               <p>Preencha os campos que deseja </p>
               <label>Id</label>
               <input {...register('id')} type="text" value={bookExist.id} disabled></input>
               {errors.id && (<span>{errors.id.message}</span>)}
               <label>Titulo</label>
-              <input {...register('title')} type="text" value={bookExist.title}></input>
+              <input {...register('title')} type="text" defaultValue={bookExist.title}></input>
               <label>Número de Páginas</label>
-              <input {...register('pages')} type="text" value={bookExist.pages}></input>
+              <input {...register('pages')} type="text" defaultValue={bookExist.pages}></input>
               {errors.pages && (<span>{errors.pages.message}</span>)}
               <label>ISBN</label>
-              <input {...register('isbn')} type="text" value={bookExist.isbn}></input>
+              <input {...register('isbn')} type="text" defaultValue={bookExist.isbn}></input>
               {errors.isbn && (<span>{errors.isbn.message}</span>)}
               <label>Editora</label>
-              <input {...register('publisher')} type="text" value={bookExist.publisher}></input>
+              <input {...register('publisher')} type="text" defaultValue={bookExist.publisher}></input>
               <label>Descrição</label>
-              <textarea {...register('sumary')} type="text" rows={5} value={bookExist.sumary}></textarea>
+              <textarea {...register('sumary')} type="text" rows={5} defaultValue={bookExist.sumary}></textarea>
               <button type='submit' className='buttondefault'>Editar Livro</button>
             </form>
           </div>
